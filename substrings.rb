903b9735725f1b429ac15771c730bff533293dce
Josh <jweiler@cn.edu>
2015-06-12 20:15:09 +0000

    Completed substrings

---
+++ substrings.rb
@@ -0,0 +1,22 @@
+def substrings(wordString, subStrings)
+  output = Hash.new()
+  wordArray = wordString.split(/\W/).reject!{|w| w.empty?}
+  puts wordArray
+  subStrings.each do |word|
+    count = 0
+    wordArray.each do |sub|
+      if word.downcase == sub.downcase
+        count += 1
+        if count == 1
+          output.merge!(word.to_sym => count)
+        else
+          output[word.to_sym] = count
+        end
+      end
+    end
+  end  
+  output
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]  
+puts substrings("It's time to go and see it, but HOW! will it go down below?",dictionary)
